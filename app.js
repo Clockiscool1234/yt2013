baeb58af3bbb80e0ae5bf3a02cdac2c37818cc47
Clockiscool1234 <85726365+Clockiscool1234@users.noreply.github.com>
2023-04-12 10:20:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const videoCardContainer = document.querySelector('.video-container');
 
-let api_key = "";
+let api_key = "AIzaSyAe-TVdpD8kck_LmkKlSJzFnGJOb2EmLrE";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
